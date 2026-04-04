0ae4c8f9144083e55b42020c88b6580df6dc0425
Cartsea Lavinia <laviniac7355@gmail.com>
2026-04-04 21:22:08 +0300

    Updated file

--- Application.cpp
+++ Application.cpp
@@ -1,24 +1,23 @@
 #include "Application.h"
 
-Application::Application(){
-    name="Unknown";
-    developer="Unknown";
-    version=0.0;
-    sizeMB=0;
+Application::Application() 
+    : name("Unknown"),
+    type("Unknown"),
+    developer("Anonymous"), 
+    version(1.0f), 
+    sizeMB(0) 
+{}
 
-}
-
-Application::Application(std::string name, std::string developer, float version, int sizeMB) 
+Application::Application(std::string name,std::string type, std::string developer, float version, int sizeMB) 
     : name(name), 
       developer(developer), 
       version(version), 
       sizeMB(sizeMB)
-{
-
-}
+{}
 
 Application::Application(const Application& other) 
     : name(other.name), 
+      type("other.type"),
       developer(other.developer), 
       version(other.version), 
       sizeMB(other.sizeMB) 
@@ -31,6 +30,7 @@ Application& Application::operator=(const Application& obj)
     if (this == &obj)
         return *this;
     (*this).name=obj.name;
+    (*this).type=obj.type;
     (*this).developer=obj.developer;
     (*this).version=obj.version;
     (*this).sizeMB=obj.sizeMB;
@@ -42,19 +42,20 @@ Application::~Application(){}
 
 std::ostream& operator<<(std::ostream& os, const Application& obj)
 {
-    os << "Application: " << obj.name<< " | Developer: " << obj.developer<< " | v" << obj.version << " | Size: " << obj.sizeMB<< "MB";
+    os << "Application: " << obj.name<<"Type: " << obj.type<< " | Developer: " << obj.developer<< " | v" << obj.version << " | Size: " << obj.sizeMB<< "MB";
     return os;
 }
 
-std::ostream& operator>>(std::istream& is, Application& obj);
+std::ostream& operator>>(std::istream& is, Application& obj)
 {
     std::cout << "Enter name: ";
     is >> obj.name;
+    std::cout << "Enter type: ";
+    is >> obj.type;
     std::cout << "Enter developer: ";
     is >> obj.developer;
     std::cout << "Enter version: ";
     is >> obj.version;
     std::cout << "Enter size: ";
-    is >> obj.sizeInMB;
-}
-        
+    is >> obj.sizeMB;
+}       
