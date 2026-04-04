c36ab9c1595357a3a8c7a5a8e1c2c56bb2d9f9cc
Cartsea Lavinia <laviniac7355@gmail.com>
2026-04-04 20:33:10 +0300

    uploaded file

---
+++ Application.h
@@ -0,0 +1,29 @@
+#ifndef APLICATIE_H
+#define APLICATIE_H
+
+#include <iostream>
+#include <string>
+
+class Application {
+    protected:
+        std::string name;
+        std::string developer;
+        float version;
+        int sizeMB;
+
+    public:
+        Application();
+        Application(std::string name, std::string developer, float version, int sizeMB);
+        Application(const Application& obj);
+        Application& operator=(const Application& obj);
+        virtual ~Application();
+        friend std::ostream& operator<<(std::ostream& os, const Application& obj);
+        friend std::ostream& operator>>(std::istream& is, Application& obj);
+        virtual void launchApp() = 0;
+        virtual void displayDetails() = 0;
+        std::string getName() const { return name; }
+        float getVersion() const {return version; }
+
+
+};
+#endif
