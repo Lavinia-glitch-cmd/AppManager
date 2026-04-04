408a9a20d07807fc7cf2534b408dd0448119a464
Cartsea Lavinia <laviniac7355@gmail.com>
2026-04-04 22:21:03 +0300

    changed constr

--- Social.h
+++ Social.h
@@ -1,30 +1,32 @@
-#ifndef SOCIALAPP_H
-#define SOCIALAPP_H
-
-#include "Social.h"
+#ifndef Social_H
+#define Social_H
+#include <iostream>
 #include <string>
-
-class Social:virtual public Social{
+#include "Application.h"
+class Social:virtual public Application{
     protected:
-        // int user;
-        int followers;
+        int users;
         std::string privacy;
         bool ads;
-        bool membership;
+        float membership;
         bool blue_check;
         std::string category;
 
     public:
         Social();
-        Social(std::string type, std::string developer, float version, int sizeMB);
+        Social(std::string n, std::string d, float v, int s, int u, std::string p, bool a, float m, bool b, std::string c);
         Social(const Social& obj);
         Social& operator=(const Social& obj);
         virtual ~Social();
         friend std::ostream& operator<<(std::ostream& os, const Social& obj);
-        friend std::ostream& operator>>(std::istream& is, Social& obj);
-        
+        friend std::istream& operator>>(std::istream& is, Social& obj);
+
+        void launch() override;
+        void displayDetails() const override;
 
 
 };
 
 #endif
+
+
