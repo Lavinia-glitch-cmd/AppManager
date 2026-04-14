dd2384644d73320c89af36484b5cf2019ec86d54
Cartsea Lavinia <laviniac7355@gmail.com>
2026-04-14 08:43:21 +0300

    declared clSS

--- Profile.h
+++ Profile.h
@@ -5,16 +5,18 @@
 #include <string>
 #include <vector>
 
+class Application;
+
 class Profile {
     protected:
         std::string username;
         std::string password;
+        Application* app;
     public:
         Profile();
         Profile(std::string u="", std::string p="");
         Profile(const Profile &obj);
         Profile& operator=(const Profile &obj);
-
         virtual ~Profile();
         friend std::ostream& operator<<(std::ostream& os, const Profile& obj);
         friend std::istream& operator>>(std::istream& is, Profile& obj);
@@ -22,6 +24,9 @@ class Profile {
 
         virtual void readData()=0;
         virtual void display() const =0;
+        void setApp(Application* app) {this-> app= app;} 
+        void createSocialAccount();
+        void createCommercialAccount();
 };
 
 #endif
